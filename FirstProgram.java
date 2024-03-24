7f093aabb30b8c301404115501d6540f86b9488f
Sakshi Balnath Bankar <105833511+sakshibalnathbankar@users.noreply.github.com>
2024-03-24 12:31:38 +0530

    Create FirstProgram.java

---
+++ FirstProgram.java
@@ -0,0 +1,9 @@
+public class FirstProgram
+{
+  public static void main(String[] args)
+  {
+    System.out.println("-------------------------------------------------");
+    System.out.println("The First program running on Jenkins from GitHub.");
+    System.out.println("-------------------------------------------------");
+  }
+}
